5fb1c3b2f412ad4f34a06b3a53c32226fb912648
Vasanth S <vasumalar1920@gmail.com>
2026-06-13 01:58:57 +0530

    Update!!!!

---
+++ fundamentals/operators.py
@@ -0,0 +1,31 @@
+# assigment operator
+
+age = 25
+
+# arithmatic operators
+
+addition = 5 + 3
+print(addition)
+
+subration  = 2-1
+print(subration)
+
+multi = 5*5
+print(multi)
+
+divi = 4/2
+print(divi)
+
+modulas = 4%3
+print(modulas)
+
+print (4**2)
+
+print(5//2)
+
+
+print("scamp"+"is a goog dog")
+
+age =8
+age +=8 # age = age+8
+print(age)
